d2e92e561302b7d77feb72fdeff2db54f14673c7
jsvirzi <jsvirzi@gmail.com>
2016-06-26 16:02:15 -0700

    added exit strategy

--- app/src/main/java/com/mam/lambo/jsvirzi/simplecameratoh264/MainActivity.java
+++ app/src/main/java/com/mam/lambo/jsvirzi/simplecameratoh264/MainActivity.java
@@ -1,8 +1,8 @@
 package com.mam.lambo.jsvirzi.simplecameratoh264;
 
 import android.os.Bundle;
+import android.os.Process;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -24,9 +24,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 cameraModule.release();
+                Process.killProcess(Process.myPid());
             }
         });
     }
